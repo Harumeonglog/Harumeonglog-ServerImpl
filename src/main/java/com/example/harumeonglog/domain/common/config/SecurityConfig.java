dd27b6f19451edb70426c8e60ee8ec5d41ce4ac7
Jeongmo Seo <wjdah1402@gmail.com>
2025-04-15 22:40:17 +0900

    :sparkles: feat: Swagger Login 특정 Scope만 적용하도록 변경

--- src/main/java/com/example/harumeonglog/domain/common/config/SecurityConfig.java
+++ src/main/java/com/example/harumeonglog/domain/common/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import com.example.harumeonglog.domain.common.auth.handler.JwtTokenLogoutHandler
 import com.example.harumeonglog.domain.common.auth.service.CustomDetailService;
 import com.example.harumeonglog.domain.common.auth.util.JwtUtil;
 import com.example.harumeonglog.domain.common.auth.util.RedisUtil;
+import com.example.harumeonglog.domain.common.config.data.ProfileConfigData;
 import com.example.harumeonglog.domain.common.config.data.SwaggerConfigData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -38,6 +39,7 @@ import org.springframework.security.web.context.SecurityContextRepository;
 public class SecurityConfig {
 
     private final SwaggerConfigData swaggerConfigData;
+    private final ProfileConfigData profileConfigData;
     private final RedisUtil redisUtil;
     private final JwtUtil jwtUtil;
     private final CustomDetailService customDetailService;
@@ -87,6 +89,13 @@ public class SecurityConfig {
     @Bean
     @Order(0)
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+        if (!swaggerConfigData.getAuthentication().getProfileScope().contains(profileConfigData.getOnProfile())) {
+
+            http.securityMatchers(matchers -> matchers.requestMatchers(apiUrl))
+                    .authorizeHttpRequests(request -> request.anyRequest().permitAll())
+            ;
+            return http.build();
+        }
         http
                 .securityMatchers(matchers -> matchers
                         .requestMatchers(apiUrl)
