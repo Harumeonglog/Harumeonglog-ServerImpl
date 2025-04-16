4062091ac4453057f8f172c7fd1d11da316d21fd
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-04-16 16:47:33 +0900

    📝 docs: swagger 명세서 분리

--- src/main/java/com/example/harumeonglog/domain/post/controller/PostController.java
+++ src/main/java/com/example/harumeonglog/domain/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.harumeonglog.domain.post.controller;
 
 import com.example.harumeonglog.domain.member.entity.Member;
 import com.example.harumeonglog.domain.post.controller.enums.PostRequestCategory;
+import com.example.harumeonglog.domain.post.controller.specification.PostControllerSpecification;
 import com.example.harumeonglog.domain.post.entity.Post;
 import com.example.harumeonglog.global.common.response.CustomResponse;
 import com.example.harumeonglog.domain.post.dto.request.PostRequest;
@@ -21,15 +22,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
 @Tag(name = "Post", description = "Post 관련 API")
-public class PostController {
+public class PostController implements PostControllerSpecification {
 
     private final PostCommandService postCommandService;
     private final PostQueryService postQueryService;
 
-    @Operation(summary = "게시물 조회 API by 김준환", description = "게시물 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다.")
-    })
+
     @GetMapping
     public CustomResponse<PostResponse.PostPreviewListResponse> getPosts(
             @RequestParam(name = "search") String search,
@@ -41,11 +39,6 @@ public class PostController {
         return CustomResponse.ok(postListResponse);
     }
 
-    @Operation(summary = "게시물 상세 조회 API by 김준환",description = "게시물 상세 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
-            @ApiResponse(responseCode = "POST404", description = "게시물을 찾지 못했습니다.")
-    })
     @GetMapping("/{postId}")
     public CustomResponse<PostResponse.PostDetailResponse> getPost(
             @PathVariable Long postId, @AuthenticatedMember Member member
@@ -54,10 +47,7 @@ public class PostController {
         return CustomResponse.ok(postDetailResponse);
     }
 
-    @Operation(summary = "게시물 생성 API by 김준환",description = "게시물 생성")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다.")
-    })
+
     @PostMapping
     public CustomResponse<Long> createPost(
             @RequestBody PostRequest.PostCreateRequest postCreateRequest
@@ -66,11 +56,6 @@ public class PostController {
         return CustomResponse.ok(post.getId());
     }
 
-    @Operation(summary = "게시물 수정 API by 김준환",description = "게시물 수정")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
-            @ApiResponse(responseCode = "POST404", description = "게시물을 찾지 못했습니다.")
-    })
     @PatchMapping("/{postId}")
     public CustomResponse<Long> updatePost(
             @PathVariable Long postId,
@@ -80,11 +65,6 @@ public class PostController {
         return CustomResponse.ok(post.getId());
     }
 
-    @Operation(summary = "게시물 삭제 API by 김준환",description = "게시물 삭제")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
-            @ApiResponse(responseCode = "POST404", description = "게시물을 찾지 못했습니다.")
-    })
     @DeleteMapping("/{postId}")
     public CustomResponse<Void> deletePost(
             @PathVariable Long postId
@@ -93,11 +73,7 @@ public class PostController {
         return CustomResponse.ok(null);
     }
 
-    @Operation(summary = "게시물 좋아요 API by 김준환",description = "게시물 좋아요 생성/삭제")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
-            @ApiResponse(responseCode = "POST404", description = "게시물을 찾지 못했습니다.")
-    })
+
     @PostMapping("/{postId}/likes")
     public CustomResponse<Void> likePost(
             @PathVariable Long postId,
@@ -107,11 +83,6 @@ public class PostController {
         return CustomResponse.ok(null);
     }
 
-    @Operation(summary = "게시물 신고 API by 김준환",description = "게시물 신고 생성/삭제")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
-            @ApiResponse(responseCode = "POST404", description = "게시물을 찾지 못했습니다.")
-    })
     @PostMapping("/{postId}/reports")
     public CustomResponse<Void> reportPost(
             @PathVariable Long postId,
@@ -121,10 +92,6 @@ public class PostController {
         return CustomResponse.ok(null);
     }
 
-    @Operation(summary = "내가 쓴 게시물 조회 API by 김준환",description = "내가 쓴 게시물 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다.")
-    })
     @GetMapping("/me")
     public CustomResponse<PostResponse.PostPreviewListResponse> getMyPost(
             @RequestParam(name = "cursor") Long cursor,
@@ -135,10 +102,6 @@ public class PostController {
         return CustomResponse.ok(postPreviewListResponse);
     }
 
-    @Operation(summary = "내가 좋아요 누른 게시물 조회 API by 김준환",description = "내가 좋아요 누른 게시물 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "성공입니다.")
-    })
     @GetMapping("/me/likes")
     public CustomResponse<PostResponse.PostPreviewListResponse> getMyLikePost(
             @RequestParam(name = "cursor") Long cursor,
