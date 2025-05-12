124b75d8098716ed218aa59024ff19565f45f8f2
bxxrxtop <bsd03291@gmail.com>
2025-05-12 17:31:34 +0900

    :hammer: S3 Controller formatting

--- src/main/java/com/example/harumeonglog/global/s3/controller/S3Controller.java
+++ src/main/java/com/example/harumeonglog/global/s3/controller/S3Controller.java
@@ -1,10 +1,11 @@
 package com.example.harumeonglog.global.s3.controller;
 
 import com.example.harumeonglog.global.common.response.CustomResponse;
-import com.example.harumeonglog.global.s3.controller.specification.S3ControllerSpecification;
 import com.example.harumeonglog.global.s3.dto.request.S3RequestDTO;
 import com.example.harumeonglog.global.s3.dto.response.S3ResponseDTO;
 import com.example.harumeonglog.global.s3.service.S3Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/s3")
 @Tag(name = "S3", description = "S3 이미지 업로드 관련 API")
-public class S3Controller implements S3ControllerSpecification {
+public class S3Controller {
 
     private final S3Service s3Service;
 
+    @Operation(summary = "S3 단일 이미지 PresignedUrl 발급 by 백종우", description = "domain에 해당하는 S3 단일 이미지의 PresignedUrl을 발급합니다.")
+    @ApiResponse(responseCode = "COMMON200", description = "발급성공")
     @PostMapping("/presigned-urls")
     public CustomResponse<S3ResponseDTO.S3ResponsePreviewDTO> getPresignedUrl(
             @RequestBody S3RequestDTO.GeneratePresignedUrlRequest request) {
         return CustomResponse.ok(s3Service.generatePresignedUrl(request));
     }
 
+    @Operation(summary = "S3 복수 이미지 PresignedUrl 발급 by 백종우", description = "domain에 해당하는 S3 복수 이미지의 PresignedUrl을 발급합니다.")
+    @ApiResponse(responseCode = "COMMON200", description = "발급성공")
     @PostMapping("/presigned-urls/batch")
     public CustomResponse<S3ResponseDTO.S3ResponseListDTO> getPresignedUrls(
             @RequestBody S3RequestDTO.GeneratePresignedUrlsRequest request) {
