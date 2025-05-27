1378c944d8f50dc0ab4e29401ce9b214525d16f9
Jeongmo Seo <wjdah1402@gmail.com>
2025-05-27 12:29:13 +0900

    :sparkles: feat: 산책 쿼리 수정

---
+++ src/main/java/com/example/harumeonglog/domain/walk/enums/WalkSort.java
@@ -0,0 +1,5 @@
+package com.example.harumeonglog.domain.walk.enums;
+
+public enum WalkSort {
+    DISTANCE, TIME, RECOMMEND
+}
--- src/main/java/com/example/harumeonglog/domain/walk/repository/WalkRepository.java
+++ src/main/java/com/example/harumeonglog/domain/walk/repository/WalkRepository.java
@@ -8,8 +8,27 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface WalkRepository extends JpaRepository<Walk, Long> {
+    Slice<Walk> findAllByIsSharedIsTrueOrderByDistanceDescIdDesc(Pageable pageable);
+
     @Query("SELECT w FROM Walk w WHERE " +
-            "w.id < :cursor AND w.isShared = true " +
+            "(w.distance < (SELECT w1.distance FROM  Walk w1 WHERE w1.id = :cursor) OR (w.distance = (SELECT w1.distance FROM  Walk w1 WHERE w1.id = :cursor) AND w.id < :cursor)) " +
+            "AND w.isShared = true " +
             "ORDER BY w.distance DESC, w.id DESC")
-    Slice<Walk> findAllByDistance(@Param("cursor") Long cursor, Pageable pageable);
+    Slice<Walk> findAllByDistanceDesc(@Param("cursor") Long cursor, Pageable pageable);
+
+    Slice<Walk> findAllByIsSharedIsTrueOrderByTimeDescIdDesc(Pageable pageable);
+
+    @Query("SELECT w FROM Walk w WHERE " +
+            "(w.time < (SELECT w1.time FROM Walk w1 WHERE w1.id = :cursor) OR (w.time = (SELECT w1.time FROM Walk w1 WHERE w1.id = :cursor) AND w.id < :cursor)) " +
+            "AND w.isShared = true " +
+            "ORDER BY w.time DESC, w.id DESC")
+    Slice<Walk> findAllByTimeDesc(@Param("cursor") Long cursor, Pageable pageable);
+
+    Slice<Walk> findAllByIsSharedIsTrueOrderByWalkLikeNumDescIdDesc(Pageable pageable);
+
+    @Query("SELECT w FROM Walk w WHERE " +
+            "(w.walkLikeNum < (SELECT w1.walkLikeNum FROM Walk w1 WHERE w1.id = :cursor) OR (w.walkLikeNum = (SELECT w1.walkLikeNum FROM Walk w1 WHERE w1.id = :cursor) AND w.id < :cursor)) " +
+            "AND w.isShared = true " +
+            "ORDER BY w.walkLikeNum DESC, w.id DESC")
+    Slice<Walk> findAllByWalkLikeNumDesc(@Param("cursor") Long cursor, Pageable pageable);
 }
