b0fe9a93e99e75db5a11236c96f50ee32ae9b816
Jeongmo Seo <wjdah1402@gmail.com>
2025-06-27 14:14:50 +0900

    :bug: fix: 산책 쿼리 수정

--- src/main/java/com/example/harumeonglog/domain/walk/repository/MemberWalkRepository.java
+++ src/main/java/com/example/harumeonglog/domain/walk/repository/MemberWalkRepository.java
@@ -3,7 +3,17 @@ package com.example.harumeonglog.domain.walk.repository;
 import com.example.harumeonglog.domain.walk.entity.MemberWalk;
 import com.example.harumeonglog.domain.walk.entity.Walk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberWalkRepository extends JpaRepository<MemberWalk, Long> {
+    @Query("""
+        SELECT mw FROM MemberWalk mw JOIN FETCH mw.member m WHERE mw.id =
+        (SELECT MIN(mw2.id) FROM MemberWalk mw2 WHERE mw2.walk.id In :walks)
+    """)
+    List<MemberWalk> findMemberNicknameByWalks(@Param("walks") List<Long> walks);
+
     MemberWalk findTopByWalkOrderByCreatedAtAsc(Walk walk);
 }
